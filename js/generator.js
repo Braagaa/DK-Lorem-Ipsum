41f9bbaeba388652bbd88bcd9089d9543ba8e65a
Braagaa <michael_braga@outlook.com>
2018-01-11 23:41:27 -0500

    Finished list of items

---
+++ js/generator.js
@@ -0,0 +1,10 @@
+const list = 
+[
+    'kong', 'donkey', 'diddy', 'cranky', 'funky', 'candy', 'dixie',
+    'wrinkly', 'tiny', 'chunky', 'kiddy', 'lanky', 'swanky', 'rambi',
+    'squawks', 'clapper', 'expresso', 'rattly', 'winky', 'squitter',
+    'ellie', 'parry', 'glimmer', 'fluri', 'helibird', 'hoofer', 'orco',
+    'chops', 'rool', 'banana', 'balloon', 'KremKoin', 'DK', 'bonus',
+    'barrel', 'watermelon', 'TNT', 'kremling'
+];
+
